303fcd425b208ac60b9da00c7da2e96bdd6a349f
Juan Aragon <156331597+juanaragong@users.noreply.github.com>
2024-02-12 18:17:44 -0600

    Add files via upload

---
+++ LineasColores.py
@@ -0,0 +1,55 @@
+from turtle import*
+import turtle
+import time
+
+
+title("Topicos Avanzados de Programacion")
+s=turtle.getscreen()
+l1=turtle.Turtle()
+contador=1
+while(contador>0):
+    l1.pensize(5)
+    l1.pencolor("Red")
+    l1.penup()
+    l1.goto(-370,-200)
+    l1.pendown()
+    l1.goto(-100,-200)
+    l1.goto(-100,-40)
+    l1.goto(-100,100)
+    l1.goto(200,100)
+    l1.goto(200,320)
+    l1.penup()
+    l1.goto(-320,320)
+    l1.pendown()
+    l1.color("Blue")
+    l1.goto(-320,200)
+    l1.goto(-160,200)
+    l1.goto(-160,50)
+    l1.goto(100,50)
+    l1.goto(100,-50)
+    l1.goto(280,-50)
+    l1.goto(280,-320)
+    l1.color("Green")
+    l1.penup()
+    l1.goto(0,-310)
+    l1.pendown()
+    l1.goto(0,-100)
+    l1.goto(-230,-100)
+    l1.goto(-230,260)
+    l1.goto(100,260)
+    l1.goto(100,190)
+    l1.goto(390,190)
+    l1.pencolor("Orange")
+    l1.penup()
+    l1.goto(-370,0)
+    l1.pendown()
+    l1.goto(-300,0)
+    l1.goto(-300,-280)
+    l1.goto(330,-280)
+    l1.goto(330,330)
+
+exitonclick()
+
+
+
+
